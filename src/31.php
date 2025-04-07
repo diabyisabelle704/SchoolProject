4e6014f3ca45cc832e6518a61e58c321cbf1933f
diabyisabelle704 <diabyisabelle704@gmail.com>
2025-04-07 14:53:10 +0000

    Add 31.php

---
+++ src/31.php
@@ -0,0 +1,11 @@
+<?php
+// Define variables
+$name = "John";
+$password = "secret";
+
+// Check if the password is correct
+if ($password === "secret") {
+    echo "Password is correct!";
+} else {
+    echo "Password is incorrect.";
+}
