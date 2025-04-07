425134a800a1ca41d8ead33f6104a31682930eb1
diabyisabelle704 <diabyisabelle704@gmail.com>
2025-04-07 14:46:35 +0000

    Add 27.php

---
+++ src/27.php
@@ -0,0 +1,11 @@
+<?php
+function addNumbers($num1, $num2) {
+    return $num1 + $num2;
+}
+
+$num1 = 5;
+$num2 = 3;
+
+$result = addNumbers($num1, $num2);
+echo "The sum of $num1 and $num2 is: " . $result;
+?>
