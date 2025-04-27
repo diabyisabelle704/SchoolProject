08feb1a6757f94b479b83228fd5e5204b99ccf0c
diabyisabelle704 <diabyisabelle704@gmail.com>
2025-04-27 11:40:15 +0000

    Add 119.php

---
+++ src/119.php
@@ -0,0 +1,16 @@
+<?php
+// This is a sample PHP script.
+// It contains some basic functions and variables.
+
+$a = 10;
+$b = 20;
+
+// Output the values of $a and $b.
+echo "Value of $a: $a, Value of $b: $b";
+
+function add($a, $b) {
+    return $a + $b;
+}
+
+$result = add($a, $b);
+echo "Result of adding $a and $b is: $result";
