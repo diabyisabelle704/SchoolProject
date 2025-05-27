17b1068f121139ba8184e64e21ac8e8e235f4214
diabyisabelle704 <diabyisabelle704@gmail.com>
2025-05-27 15:36:32 +0000

    Add 127.php

---
+++ src/127.php
@@ -0,0 +1,10 @@
+<?php
+// Define your variables here
+$variable1 = "value1";
+$variable2 = "value2";
+
+// Perform some operations with the variables
+$result = $variable1 + $variable2;
+
+echo $result;
+?>
